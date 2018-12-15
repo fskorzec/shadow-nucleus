51b15e8eded6b827664d8657ba41adfef03b07dc
Skorzec francois <fskorzec@outlook.com>
2018-12-15 14:33:24 -0500

    Starting refactoring

---
+++ scripts/tools.d.ts
@@ -0,0 +1,3 @@
+export function mkDirSync(source: string): void;
+export function copySync(source: string, target: string): void;
+export function rmDirSync(source: string): void;
--- src/Cli.ts
+++ src/Cli.ts
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
-console.log("ok");
+import * as tools from "../scripts/tools";
+
+const currentPath = __dirname;
+const cwdPath = process.cwd();
+
+tools.copySync(`${currentPath}/../scripts`,`${cwdPath}/nc-scripts`);
+tools.copySync(`${currentPath}/../dist/_packages`,`${cwdPath}/nc-packages`);
+console.log("done");
