5f782b0b852fb120c76cbe4744e832ae778311cf
Skorzec francois <fskorzec@outlook.com>
2019-01-14 23:59:28 -0500

    added build

--- src/CliBuild.ts
+++ src/CliBuild.ts
@@ -1,2 +1,15 @@
+#!/usr/bin/env node
 import * as tools from "../scripts/tools";
 
+const params = process.argv;
+params.shift();
+params.shift();
+
+const modulePath = params.shift();
+const moduleName = params.shift();
+const targetPath = params.shift();
+const version    = params.shift();
+const targetMode = params.shift();
+
+console.log(tools.execSync(`tsc ./${modulePath}/${moduleName}.ts ./out/${targetPath}`).toString());
+console.log(tools.execSync(`yarn webpack --entry ./out/${targetPath}/${moduleName}.js --output ./dist/${moduleName}/${version}/${targetMode === "web" ? "front" : "back"}/${moduleName}.js --target ${targetMode}`).toString());
